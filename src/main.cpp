9e7e9b78aa3f05e907b8f2765eeda6b9edce828f
Abraamyan Alexander <hryapusek@gmail.com>
2023-11-18 02:53:24 +0300

    upd: conditions were moved to conditions files

--- src/main.cpp
+++ src/main.cpp
@@ -4,74 +4,9 @@
 #include "bdd.h"
 #include "BDDHelper.hpp"
 #include "BDDFormulaBuilder.hpp"
-
-/*
-   Украинец живет в доме красного цвета.
-   Значит должен быть хотя бы один дом, где живет украинец и красный цвет
- */
+#include "Conditions.hpp"
 
 using namespace bddHelper;
-
-void addLoopCondition(bdd formula, BDDFormulaBuilder &builder, BDDHelper &h);
-void addFirstCondition(BDDHelper &h, BDDFormulaBuilder &builder);
-void addSecondCondition(BDDHelper &h, BDDFormulaBuilder &builder);
-void addThirdCondition(BDDHelper &h, BDDFormulaBuilder &builder);
-void addFourthCondition(BDDHelper &h, BDDFormulaBuilder &builder);
-void addFifthCondition(BDDHelper &h, BDDFormulaBuilder &builder);
-
-void addLoopCondition(bdd formula, BDDFormulaBuilder &builder, BDDHelper &h)
-{
-  auto formulaToAdd = bdd_true();
-  for (auto i : std::views::iota(0, 5))
-  {
-    auto house = static_cast< House >(i);
-    formulaToAdd |= (h.getHouse(house) & formula);
-  }
-  builder.addCondition(formulaToAdd);
-}
-
-void addFirstCondition(BDDHelper &h, BDDFormulaBuilder &builder)
-{
-  auto ukraineNation = h.getPropVal(Nation::UKRAINE);
-  auto redHouse = h.getPropVal(H_Color::RED);
-  addLoopCondition(ukraineNation & redHouse, builder, h);
-}
-
-void addSecondCondition(BDDHelper &h, BDDFormulaBuilder &builder)
-{
-  auto belarus = h.getPropVal(Nation::BELORUS);
-  auto dog = h.getPropVal(Animal::DOG);
-  addLoopCondition(belarus & dog, builder, h);
-}
-
-void addThirdCondition(BDDHelper &h, BDDFormulaBuilder &builder)
-{
-  auto greenHouse = h.getPropVal(H_Color::GREEN);
-  auto malina = h.getPropVal(Plant::MALINA);
-  addLoopCondition(greenHouse & malina, builder, h);
-}
-
-void addFourthCondition(BDDHelper &h, BDDFormulaBuilder &builder)
-{
-  auto gruzin = h.getPropVal(Nation::GRUZIN);
-  auto vinograd = h.getPropVal(Plant::VINOGR);
-  addLoopCondition(gruzin & vinograd, builder, h);
-}
-
-void addFifthCondition(BDDHelper &h, BDDFormulaBuilder &builder)
-{
-  auto greenHouse = h.getPropVal(H_Color::GREEN);
-  auto whiteHouse = h.getPropVal(H_Color::WHITE);
-  auto formulaToAdd = bdd_true();
-  for (auto i : std::views::iota(0, 4))
-  {
-    auto greenHouseNum = static_cast< House >(i + 1);
-    auto whiteHouseNum = static_cast< House >(i);
-    formulaToAdd |= (h.getHouse(whiteHouseNum) & whiteHouse
-                     & h.getHouse(greenHouseNum) & greenHouse);
-  }
-  builder.addCondition(formulaToAdd);
-}
 
 /**
  * @return std::vector< bdd > with [ o2 o1 o0 p2 p1 p0 v2 v1 v0 ]
@@ -95,6 +30,8 @@ int main()
   std::vector< bdd > vars = createVariables();
   BDDHelper h(vars);
   BDDFormulaBuilder builder;
+  conditions::addConditions(h, builder);
+  std::cout << bddset << builder.result() << '\n';
   bdd_done();
   return 0;
 }
