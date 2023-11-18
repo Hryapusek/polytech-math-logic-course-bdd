ce17c6b15e9d960e37756d995853084e24fec0ed
Abraamyan Alexander <hryapusek@gmail.com>
2023-11-18 17:43:32 +0300

    fix: vars declared static

---
+++ src/TestFixture.cpp
@@ -0,0 +1,7 @@
+#include "TestFixture.hpp"
+
+std::vector< bdd > VarsSetupFixture::vars;
+std::vector< bdd > VarsSetupFixture::objs;
+std::vector< bdd > VarsSetupFixture::props;
+std::vector< bdd > VarsSetupFixture::vals;
+bddHelper::BDDHelper VarsSetupFixture::h;
--- src/TestFixture.hpp
+++ src/TestFixture.hpp
@@ -11,23 +11,24 @@ public:
   static constexpr int nPropsVars = bddHelper::BDDHelper::nPropsVars;
   static constexpr int nValuesVars = bddHelper::BDDHelper::nValuesVars;
   static constexpr int nTotalVars = bddHelper::BDDHelper::nTotalVars;
-  std::vector< bdd > vars;
-  std::vector< bdd > objs;
-  std::vector< bdd > props;
-  std::vector< bdd > vals;
-  bddHelper::BDDHelper h;
+  static std::vector< bdd > vars;
+  static std::vector< bdd > objs;
+  static std::vector< bdd > props;
+  static std::vector< bdd > vals;
+  static bddHelper::BDDHelper h;
 
 protected:
   virtual void SetUp()
   {
     using namespace bddHelper;
     vars = std::vector< bdd > (nTotalVars);
-    std::generate(std::begin(vars), std::end(vars),
-      []() {
-      static int i = 0;
-      auto var = bdd_ithvar(i);
-      return bdd_ithvar(i++);
-    });
+    {
+      int i = 0;
+      std::generate(std::begin(vars), std::end(vars),
+        [&i]() {
+        return bdd_ithvar(i++);
+      });
+    }
     objs = std::vector< bdd > (vars.begin(), vars.begin() + nObjs);
     props = std::vector< bdd > (vars.begin() + nObjsVars, vars.begin() + nObjsVars + nPropsVars);
     vals = std::vector< bdd > (vars.begin() + nObjsVars + nPropsVars, vars.end());
